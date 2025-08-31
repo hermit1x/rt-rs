506c98c383cec5599f2e3d3b653b0dc9ba94b0ec
hermit1x <1242291955@qq.com>
2025-08-31 14:54:44 -0700

    update: Use a faster method for common::random_unit_vec3()

--- src/common.rs
+++ src/common.rs
@@ -1,16 +1,17 @@
 use rand::Rng;
 use rand::rngs::ThreadRng;
+use rand_distr::{Normal, Distribution};
 
 pub type Point3 = nalgebra::Vector3<f64>;
 pub type Vec3 = nalgebra::Vector3<f64>;
 pub type Color = nalgebra::Vector3<f64>;
 
 pub fn random(rnd: &mut ThreadRng) -> f64 {
-    rnd.gen_range(0.0..=1.0)
+    rnd.random_range(0.0..=1.0)
 }
 
 pub fn random_range(min: f64, max: f64, rnd: &mut ThreadRng) -> f64 {
-    rnd.gen_range(min..=max)
+    rnd.random_range(min..=max)
 }
 
 pub fn random_vec3(rnd: &mut ThreadRng) -> Vec3 {
@@ -22,11 +23,11 @@ pub fn random_range_vec3(min: f64, max: f64, rnd: &mut ThreadRng) -> Vec3 {
 }
 
 pub fn random_unit_vec3(rnd: &mut ThreadRng) -> Vec3 {
-    let mut v = random_range_vec3(-1.0, 1.0, rnd);
-    while v.norm() > 1.0 {
-        v = random_range_vec3(-1.0, 1.0, rnd);
-    }
-    v.normalize()
+    let mut normal_dist = Normal::new(0.0, 1.0).unwrap();
+    let x = normal_dist.sample(rnd);
+    let y = normal_dist.sample(rnd);
+    let z = normal_dist.sample(rnd);
+    Vec3::new(x, y, z).normalize()
 }
 
 pub fn random_on_hemisphere(normal: &Vec3, rnd: &mut ThreadRng) -> Vec3 {
